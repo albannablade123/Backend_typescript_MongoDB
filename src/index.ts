994a19cd32eb8854887de5da9b205256757f58e5
albannablade123 <hasanal082000@gmail.com>
2022-09-22 16:26:56 +0300

    - Added authentication middleware

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,14 @@ import 'dotenv/config';
 import App from './app';
 import validateEnv from '@/utils/validateEnv';
 import PostController from './resources/post/post.controller';
-
+import UserController from  '@/resources/user/user.controller';
 //validateEnv();
 
 console.log("reached here");
 
 const app = new App([
-    new PostController()
+    new PostController(),
+    new UserController()
 ], Number(process.env.PORT));
 
 app.listen();
--- src/resources/user/user.controller.ts
+++ src/resources/user/user.controller.ts
@@ -1,4 +1,4 @@
-import {Router, Request, Response, NextFunction} from "express";
+import { Router, Request, Response, NextFunction } from "express";
 import Controller from "@/utils/interfaces/controller.interface";
 import HttpException from "@/utils/exceptions/http.exception";
 import validationMiddleware from "@/middleware/validation.middleware";
@@ -6,25 +6,77 @@ import validate from "@/resources/user/user.validation";
 import UserService from "./user.service";
 import authenticated from "@/middleware/authenticated.middleware";
 
-class UserController implements Controller{
-    public path = '/users';
-    public router = Router();
-    private UserService = new UserService();
+class UserController implements Controller {
+  public path = "/users";
+  public router = Router();
+  private UserService = new UserService();
 
-    constructor(){
-        this.initialiseRoutes();
+  constructor() {
+    this.initialiseRoutes();
+  }
+
+  private initialiseRoutes() {
+    this.router.post(
+      `${this.path}/register`,
+      validationMiddleware(validate.register, this.register)
+    );
+
+    this.router.post(
+      `${this.path}/login`,
+      validationMiddleware(validate.login, this.login)
+    );
+    this.router.get(`${this.path}`, authenticated, this.getUser);
+  }
+  private register = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<Response | void> => {
+    try {
+      const { firstName, lastName, email, password } = req.body;
+      const token = await this.UserService.register(
+        firstName,
+        lastName,
+        email,
+        password,
+        "user"
+      );
+
+      //Indicate that resource is succesfully created and return a token
+      res.status(201).json({ token });
+    } catch (error: any) {
+      next(new HttpException(400, error.message));
+    }
+  };
+
+  private login = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<Response | void> => {
+    try {
+      const { email, password } = req.body;
+
+      const token = await this.UserService.login(email, password);
+
+      res.status(201).json({ token });
+    } catch (error: any) {
+      next(new HttpException(400, error.message));
+    }
+  };
+
+  private getUser = (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ) : Response | void => {
+    if (!req.user) {
+        return next(new HttpException(404,'No logged in user'))
     }
 
-    private initialiseRoutes(){
-        this.router.post(
-            `${this.path}/register`,
-            validationMiddleware(validate.register,
-                this.register)
-        )
+    res.status(200).json({user: req.user});
 
-        this.router.post(
-            `${this.path}/login`,
-            validationMiddleware(validate.login,this.login)
-        )
-    }
+  }
 }
+
+export default UserController;
